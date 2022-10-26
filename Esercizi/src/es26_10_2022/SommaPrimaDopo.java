c7fe042a0ba13807b0e0d87ee285a06b480cde95
runzac <91915526+runzac@users.noreply.github.com>
2022-10-26 18:29:04 +0200

    Update SommaPrimaDopo.java

--- Esercizi/src/es26_10_2022/SommaPrimaDopo.java
+++ Esercizi/src/es26_10_2022/SommaPrimaDopo.java
@@ -7,6 +7,7 @@ public class SommaPrimaDopo {
 		int[] array = new int[10];
 		//int[] array = {8, 0, -4, 11, 5, 9, 4, 6, 2, -8};
 		int somma = 0;
+		boolean risp = false;
 		
 		for(int i = 0; i<10; i++) {
 			array[i] = (int) (Math.random()*10);
@@ -23,13 +24,15 @@ public class SommaPrimaDopo {
 			//System.out.println(somma);
 			if(array[i]==somma) {
 				//System.out.println(array[i] + " | " + somma + " - OK");
-				System.out.println("Pos: " + i + " OK: " + somma + " = " + array[i-1] + " + " + array[i+1]);
-				j++;
+				System.out.print("Pos: " + i + " " + somma + " = " + array[i-1] + " + " + array[i+1] + "\n");
+				risp = true;
 			}
 		}
-		
-		if(j==0) {
+
+		if(risp) {
+			System.out.print("OK");
+		} else {
 			System.out.println("NO");
 		}
-	}
+	}	
 }
