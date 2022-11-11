1f8e87114d3a285dba766eed532a0e68fbacdadd
runzac <91915526+runzac@users.noreply.github.com>
2022-11-11 16:40:35 +0100

    MainPunto & Punto done

---
+++ Esercizi/src/es11_11_2022/Punto.java
@@ -0,0 +1,28 @@
+package es11_11_2022;
+
+public class Punto {
+	private double x, y;
+	
+	public Punto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void LocalizzaQuadrante() {
+		if((y>0) && (x>0)) {
+			System.out.println("P (" + x + ", " + y + ")" + "\nQuesti PUNTI si trovano nel 1° Quadrante");
+		}
+		if((y>0) && (x<0)) {
+			System.out.println("P (" + x + ", " + y + ")" + "\nQuesti PUNTI si trovano nel 2° Quadrante");
+		}
+		if((y<0) && (x<0)) {
+			System.out.println("P (" + x + ", " + y + ")" + "\nQuesti PUNTI si trovano nel 3° Quadrante");
+		}
+		if((y<0) && (x>0)) {
+			System.out.println("P (" + x + ", " + y + ")" + "\nQuesti PUNTI si trovano nel 4° Quadrante");
+		}
+		if((x==0) && (y==0)) {
+			System.out.println("P (" + x + ", " + y + ")" + "\nQuesti PUNTI si trovano all'Origine");
+		}
+	}
+}
