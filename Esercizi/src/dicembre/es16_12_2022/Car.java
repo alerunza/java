204b5589397dc5355fde823ad18d8dc1a8587ed6
runzac <91915526+runzac@users.noreply.github.com>
2022-12-16 23:44:36 +0100

    MainCar & Car

--- Esercizi/src/dicembre/es16_12_2022/Car.java
+++ Esercizi/src/dicembre/es16_12_2022/Car.java
@@ -1,5 +1,23 @@
 package dicembre.es16_12_2022;
 
 public class Car {
-
+	public double chilometri;
+	private double carburante;
+	
+	public Car (double autonomia, double benzina) {
+		this.chilometri = autonomia;
+		this.carburante = benzina;
+	}
+	
+	public void Drive (double km) {
+		this.carburante = this.carburante - (km / this.chilometri);
+	}
+	
+	public double getCarburante () {
+		return this.carburante;
+	}
+	
+	public void addGas(double carburantenew) {
+		this.carburante = this.carburante + carburantenew;
+	}
 }
--- Esercizi/src/dicembre/es16_12_2022/MainCar.java
+++ Esercizi/src/dicembre/es16_12_2022/MainCar.java
@@ -3,8 +3,13 @@ package dicembre.es16_12_2022;
 public class MainCar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Car cvpi = new Car(15, 5);
+		
+		cvpi.Drive(60);
+		System.out.println(cvpi.getCarburante());
+		cvpi.addGas(20);
+		cvpi.Drive(120);
+		System.out.println(cvpi.getCarburante());
 	}
 
 }
