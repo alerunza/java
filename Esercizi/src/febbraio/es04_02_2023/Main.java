cbfc10cebc3c6bfe168057b4912fc97576557604
runzac <91915526+runzac@users.noreply.github.com>
2023-02-05 10:58:42 +0100

    Updated Main

--- Esercizi/src/febbraio/es04_02_2023/Main.java
+++ Esercizi/src/febbraio/es04_02_2023/Main.java
@@ -1,12 +1,17 @@
 package febbraio.es04_02_2023;
 
-public class MainPoint {
+public class Main {
 
 	public static void main(String[] args) {
 		Point punti = new Point(3, 4);
 		punti.checkXY(1, 1);
 		punti.moveX(2);
 		punti.checkXY(2, 1);
+		
+		Circle cerchio = new Circle(2, 5, 8);
+		cerchio.checkCircle(8);
+		cerchio.scaleR(3);
+		cerchio.checkCircle(8);
 	}
 
 }
