12f21397676687764af8fd69cad325aaf6cc68df
runzac <91915526+runzac@users.noreply.github.com>
2023-02-10 12:16:11 +0100

    First GUI Created by me (awful)

---
+++ GUI/src/GUITest/GUI.java
@@ -0,0 +1,58 @@
+package GUITest;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+//prende tutti i metodi da ActionListener...
+public class GUI implements ActionListener {
+	
+	private int conta = 0;
+	
+	private JFrame frame;
+	private JLabel label;
+	private JPanel panel;
+	
+	public GUI() {
+		frame = new JFrame();
+		
+		JButton button = new JButton("Cliccami");
+		button.addActionListener(this);
+		
+		label = new JLabel("Clicks: 0");
+		
+		panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		panel.add(label);
+		
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Chubs");
+		frame.pack();
+		frame.setVisible(true);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		new GUI();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		conta++;
+		label.setText("Clicks: " + conta);
+		
+	}
+
+}
---
+++ GUI/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author alerunza
+ *
+ */
+module GUI {
+	requires java.desktop;
+}
