6c3d71d3d9fc4dcd57b31e15a91e4f25d9ea9735
runzac <91915526+alerunza@users.noreply.github.com>
2023-05-20 16:08:43 +0200

    ES.1 (Gestione Pulsanti) - add ActionListener

--- Esercizi/src/maggio/es20_05_2023/MainGestionePulsanti.java
+++ Esercizi/src/maggio/es20_05_2023/MainGestionePulsanti.java
@@ -2,24 +2,33 @@ package maggio.es20_05_2023;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainGestionePulsanti {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 200);
+        frame.setSize(450, 300);
         frame.setLocation(100, 100);
 
         JPanel panel = new JPanel(new BorderLayout());
         panel.setBackground(Color.GREEN);
 
         JButton btn1 = new JButton("Superiore"), btn2 = new JButton("Inferiore");
-        JTextArea textArea = new JTextArea();
+        JTextArea textArea = new JTextArea(50, 10);
+
+        // Non è scrivibile la TextArea
+        textArea.setEditable(false);
 
         panel.add(btn1, BorderLayout.NORTH);
         panel.add(textArea, BorderLayout.CENTER);
         panel.add(btn2, BorderLayout.SOUTH);
-        frame.add(panel);
+
+        btn1.addActionListener(new GestorePulsante(textArea));
+        btn2.addActionListener(new GestorePulsante(textArea));
+
+        frame.getContentPane().add(panel);
 
         frame.setVisible(true);
     }
