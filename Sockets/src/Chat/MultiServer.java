695d1067860b8f4e1808c5d5da45375f21abd185
runzac <91915526+alerunza@users.noreply.github.com>
2023-10-31 15:21:43 +0100

    init new project

---
+++ Sockets/src/Chat/MultiServer.java
@@ -0,0 +1,31 @@
+package Chat;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MultiServer {
+ 
+	public void avviaServer() {
+		try {
+			ServerSocket server = new ServerSocket(9999);
+			for(;;) {
+				System.out.println("Server in attesa...");
+				Socket socket = server.accept();
+				System.out.println("Server socket  " + socket); 
+		        ServerThread serverThread = new ServerThread(socket);
+		        serverThread.start(); 
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		    System.out.println("Errore durante l'istanza del server !");
+		    System.exit(1);
+		}
+	}
+	
+	 public static void main (String[] args){ 
+	     MultiServer multiServer = new MultiServer(); 
+	     multiServer.avviaServer(); 
+	   } 
+}
--- Sockets/src/ChatTest/Client.java
+++ Sockets/src/ChatTest/Client.java
@@ -1,4 +1,4 @@
-package Chat;
+package ChatTest;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
