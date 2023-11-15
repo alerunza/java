9d1363d16df1e265cdec4e9b536ac91f97bfe0d8
runzac <91915526+alerunza@users.noreply.github.com>
2023-11-15 21:37:28 +0100

    es5: done

--- Sockets/src/es5/Client.java
+++ Sockets/src/es5/Client.java
@@ -40,43 +40,25 @@ public class Client {
         try {
             int num1 = 0, num2 = 0;
             String risposta = "";
-            do{
-                do {
-                    System.out.println("Quale area vuoi calcolare?\nQ - Quadrato\nR - Rettangolo");
-                    risposta = tastiera.nextLine().toLowerCase();
-                    if (!risposta.equals("q") && !risposta.equals("r")) {
-                        System.out.println("Comando errato, inserisci Q/R");
-                    } else{
-                        topolino.writeUTF(risposta);
-                    }
-                } while (!risposta.equals("q") && !risposta.equals("r"));
-                if(risposta.equals("q")){
-                    tastiera.nextLine();
-                    System.out.println("Inserisci il LATO del tuo Quadrato:");
-                    num1 = tastiera.nextInt();
-                    topolino.writeInt(num1);
-                    int numServer = this.paperina.readInt();
-                    System.out.println("dal SERVER, ecco la tua area del Quadrato: " + numServer);
-                }
-                if(risposta.equals("r")){
-                    tastiera.nextLine();
-                    System.out.println("Inserisci BASE e ALTEZZA del tuo Rettangolo:");
-                    num1 = tastiera.nextInt();
-                    num2 = tastiera.nextInt();
-                    topolino.writeInt(num1);
-                    topolino.writeInt(num2);
-                    int numServer = this.paperina.readInt();
-                    System.out.println("dal SERVER, ecco la tua area del Rettangolo: " + numServer);
-                }
-                tastiera.nextLine();
-                do {
-                    System.out.println("Vuoi continuare a calcolare? si/no");
-                    risposta = tastiera.nextLine().toLowerCase();
-                    if (!risposta.equals("si") && !risposta.equals("no")) {
-                        System.out.println("Comando errato, inserisci si/no");
-                    }
-                } while (!risposta.equals("si") && !risposta.equals("no"));
-            }while(risposta.equals("si"));
+            System.out.println("Quale area vuoi calcolare?\nQ - Quadrato\nR - Rettangolo");
+            risposta = tastiera.nextLine().toLowerCase();
+            topolino.writeUTF(risposta);
+            if(risposta.equals("q")){
+                System.out.println("Inserisci il LATO del tuo Quadrato:");
+                num1 = tastiera.nextInt();
+                topolino.writeInt(num1);
+                int numServer = this.paperina.readInt();
+                System.out.println("dal SERVER, ecco la tua area del Quadrato: " + numServer);
+            }
+            if(risposta.equals("r")){
+                System.out.println("Inserisci BASE e ALTEZZA del tuo Rettangolo:");
+                num1 = tastiera.nextInt();
+                num2 = tastiera.nextInt();
+                topolino.writeInt(num1);
+                topolino.writeInt(num2);
+                int numServer = this.paperina.readInt();
+                System.out.println("dal SERVER, ecco la tua area del Rettangolo: " + numServer);
+            }
 
             System.out.println("Passo e chiudo");
             client.close();
--- Sockets/src/es5/Server.java
+++ Sockets/src/es5/Server.java
@@ -23,27 +23,22 @@ public class Server {
 
             //comunico
             int num1 = 0, num2 = 0, operazione = 0;
-            String strRicevuta = "", strRicevuta2 = "";
-            while (!strRicevuta.equals("no")) {
-                strRicevuta2 = ins.readUTF();
-                System.out.println("strRicevuta2: " + strRicevuta2);
-                if (strRicevuta2.equals("q")) {
-                    num1 = ins.readInt();
-                    operazione = num1 * num1;
-                    System.out.println("Area Quadrato: " + num1 + " * " + num1 + " = " + operazione);
-                    outs.writeInt(operazione);
-                }
-                if (strRicevuta2.equals("r")) {
-                    num1 = ins.readInt();
-                    num2 = ins.readInt();
-                    operazione = num1 * num2;
-                    System.out.println("Area Rettangolo: " + num1 + " * " + num2 + " = " + operazione);
-                    outs.writeInt(operazione);
-                }
-                strRicevuta = ins.readUTF();
-                System.out.println("Comando ricevuto: " + strRicevuta);
+            String strRicevuta = "";
+            strRicevuta = ins.readUTF();
+            System.out.println("Comando Ricevuto: " + strRicevuta);
+            if (strRicevuta.equals("q")) {
+                num1 = ins.readInt();
+                operazione = num1 * num1;
+                System.out.println("Area Quadrato: " + num1 + " * " + num1 + " = " + operazione);
+                outs.writeInt(operazione);
             }
-
+            if (strRicevuta.equals("r")) {
+                num1 = ins.readInt();
+                num2 = ins.readInt();
+                operazione = num1 * num2;
+                System.out.println("Area Rettangolo: " + num1 + " * " + num2 + " = " + operazione);
+                outs.writeInt(operazione);
+            }
 
             //fine comunicazione - chiudo la connessione
             System.out.println("Chiudo la connessione... Ciao ciao");
