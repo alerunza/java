c06e5153426cc37a06da5ced5fbcd7e8f7b90f9d
runzac <91915526+alerunza@users.noreply.github.com>
2023-11-23 11:11:18 +0100

    boilerplate Java

---
+++ Sockets/src/boilerplateJava/Server.java
@@ -0,0 +1,26 @@
+package boilerplateJava;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(9999);
+            System.out.println("Server ON");
+            Socket client = server.accept();
+            System.out.println("Client ON");
+            server.close();
+
+            DataInputStream inp = new DataInputStream(client.getInputStream());
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+
+            System.out.println("Server - " + inp.readInt());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
