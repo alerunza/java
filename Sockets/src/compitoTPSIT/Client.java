4b3e87f0883108cc025dbb237c330f713a68031a
runzac <91915526+alerunza@users.noreply.github.com>
2023-11-24 15:02:46 +0100

    compitoTPSIT init

---
+++ Sockets/src/compitoTPSIT/Client.java
@@ -0,0 +1,45 @@
+package compitoTPSIT;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    Socket client;
+    DataInputStream inp;
+    DataOutputStream out;
+
+    public void connect() {
+        try {
+            client = new Socket("localhost", 9999);
+            inp = new DataInputStream(client.getInputStream());
+            out = new DataOutputStream(client.getOutputStream());
+
+            System.out.println("Server - Client ON");
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void communicate(){
+        Scanner input = new Scanner(System.in);
+        try {
+            int num = 3;
+            out.writeInt(num);
+            client.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.connect();
+        client.communicate();
+    }
+}
---
+++ Sockets/src/compitoTPSIT/Server.java
@@ -0,0 +1,30 @@
+package compitoTPSIT;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(9999);
+            System.out.println("Server ON");
+            Socket client = server.accept();
+            System.out.println("Client ON");
+            server.close();
+
+            DataInputStream inp = new DataInputStream(client.getInputStream());
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+
+            System.out.println("Server - " + inp.readInt());
+
+            inp.close();
+            out.close();
+            client.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
