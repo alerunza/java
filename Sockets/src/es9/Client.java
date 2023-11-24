1634dad2622fa6999bf189de142d8b7410264ff3
runzac <91915526+alerunza@users.noreply.github.com>
2023-11-24 15:02:31 +0100

    es9 init

---
+++ Sockets/src/es9/Client.java
@@ -0,0 +1,73 @@
+package es9;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    Socket client;
+    DataInputStream inp;
+    DataOutputStream out;
+
+    public void connect() {
+        try {
+            client = new Socket("localhost", 9999);
+            inp = new DataInputStream(client.getInputStream());
+            out = new DataOutputStream(client.getOutputStream());
+
+            System.out.println("Server - Client ON");
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void communicate(){
+        Scanner input = new Scanner(System.in);
+        try {
+            String risposta = "", verificaDalServer = "";
+            int tentN = 0;
+            do{
+                System.out.println("Sei pronto per giocare? digita: si");
+                risposta = input.nextLine().toLowerCase();
+                if(risposta.equals("si")){
+                    out.writeUTF(risposta);
+                }
+            }while(!risposta.equals("si"));
+
+            System.out.println("E' iniziato il gioco!\nIndovina un NUMERO tra 1 e 30\nPer arrenderti digita 0");
+            do {
+                tentN = input.nextInt();
+                out.writeInt(tentN);
+
+                verificaDalServer = inp.readUTF();
+                System.out.println(verificaDalServer);
+                if(verificaDalServer.equals("Errato")){
+                    System.out.println("Non hai indovinato! Ritenta");
+                }
+                if(verificaDalServer.equals("Vittoria")){
+                    System.out.println("Hai vinto! il NUMERO era: " + tentN);
+                }
+                if(verificaDalServer.equals("Arreso")){
+                    System.out.println("Ti sei arreso!");
+                }
+            }while (!verificaDalServer.equals("Vittoria") && !verificaDalServer.equals("Arreso"));
+
+            input.nextLine();
+            client.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.connect();
+        client.communicate();
+    }
+}
