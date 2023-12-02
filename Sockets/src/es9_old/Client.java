96d958f4569ce4d00221a6edc6c61640ce68e537
runzac <91915526+alerunza@users.noreply.github.com>
2023-12-02 10:35:53 +0100

    es9 old

---
+++ Sockets/src/es9_old/Client.java
@@ -0,0 +1,77 @@
+package es9_old;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    Socket client;
+    DataInputStream inp;
+    DataOutputStream out;
+
+    public void connect() {
+        try {
+            client = new Socket("localhost", 9999);
+            inp = new DataInputStream(client.getInputStream());
+            out = new DataOutputStream(client.getOutputStream());
+
+            System.out.println("Server - Client ON");
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void communicate(){
+        Scanner input = new Scanner(System.in);
+        try {
+            String risposta = "", continua = "", verificaDalServer = "";
+            int num = 0, tentativi;
+
+            do{
+                System.out.println("Sei pronto per giocare? digita: start");
+                risposta = input.nextLine().toLowerCase();
+                if(risposta.equals("start")){
+                    out.writeUTF(risposta);
+                } else{
+                    System.out.println("Inserisci un comando valido");
+                }
+            }while(!risposta.equals("start"));
+
+            System.out.println("E' iniziato il gioco!\nIndovina un NUMERO tra 1 e 30\nPer arrenderti digita 0");
+            tentativi = 0;
+            do {
+                num = input.nextInt();
+                tentativi++;
+                out.writeInt(num);
+                verificaDalServer = inp.readUTF();
+                System.out.println("Server Status: " + verificaDalServer);
+                if(verificaDalServer.equals("Errato")){
+                    System.out.println("Non hai indovinato! Ritenta");
+                }
+                if(verificaDalServer.equals("Vittoria")){
+                    System.out.println("Hai vinto! con " + tentativi + " tentativi, il NUMERO era: " + num);
+                }
+                if(verificaDalServer.equals("Arreso")){
+                    System.out.println("Ti sei arreso!");
+                }
+            }while (!verificaDalServer.equals("Vittoria") && !verificaDalServer.equals("Arreso"));
+
+            System.out.println("Gioco Terminato");
+
+            client.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.connect();
+        client.communicate();
+    }
+}
---
+++ Sockets/src/es9_old/Server.java
@@ -0,0 +1,55 @@
+package es9_old;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(9999);
+            System.out.println("Server ON");
+            Socket client = server.accept();
+            System.out.println("Client ON");
+            server.close();
+
+            DataInputStream inp = new DataInputStream(client.getInputStream());
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+
+            int randomNumber = 0, rispClient = 0;
+            String cmdClient = "";
+
+            cmdClient = inp.readUTF();
+            System.out.println("COMANDO ricevuto: " + cmdClient);
+
+            if(cmdClient.equals("start")){
+                randomNumber = (int) (Math.random() * 31);
+                System.out.println("NUMERO generato: " + randomNumber);
+            }
+
+            do{
+                rispClient = inp.readInt();
+                System.out.println("NUMERO ricevuto dal client: " + rispClient);
+                if(rispClient != randomNumber && (rispClient != 0)){
+                    System.out.println("Il Client ha sbagliato!");
+                    out.writeUTF("Errato");
+                }
+                if(rispClient == randomNumber){
+                    System.out.println("Il Client ha vinto! Il NUMERO era: " + randomNumber);
+                    out.writeUTF("Vittoria");
+                }
+                if(rispClient == 0){
+                    System.out.println("Il Client si è arreso!");
+                    out.writeUTF("Arreso");
+                }
+            }while (rispClient != randomNumber && rispClient != 0);
+            inp.close();
+            out.close();
+            client.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
